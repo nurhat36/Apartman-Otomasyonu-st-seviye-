0b8f7d9ca1230e7087339ede619a49e98dac1f4e
Nurhat Baydağ <baydagnurhat31@gmail.com>
2024-11-11 10:25:16 +0300

    sqlhelper eklendi 3

--- ApartmanOtomasyonu/src/main/java/com/mycompany/apartmanotomasyonu/ApartmanOtomasyonu.java
+++ ApartmanOtomasyonu/src/main/java/com/mycompany/apartmanotomasyonu/ApartmanOtomasyonu.java
@@ -10,47 +10,7 @@ public class ApartmanOtomasyonu {
 
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-=======
-       
-        
-        
-        
-        // Bağlantı bilgilerigre
-        String url = "jdbc:sqlserver://DESKTOP-BT77ME7;databaseName=APARTMAN;integratedSecurity=True;encrypt=True;trustServerCertificate=True";
-        Connection connection = null;
-        Statement statement = null;
-        ResultSet resultSet = null;
 
-        try {
-            // JDBC Sürücüsünü yükle
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-
-            // Bağlantıyı başlat
-            connection = DriverManager.getConnection(url);
-            System.out.println("Bağlantı başarılı!");
-            
-
-            // Sorguyu çalıştır
-            
-        } catch (ClassNotFoundException e) {
-            // Sürücü yükleme hatası
-            System.err.println("SQL Server JDBC sürücüsü bulunamadı.");
-            e.printStackTrace();
-        } catch (SQLException e) {
-            // Bağlantı hatası
-            e.printStackTrace();
-        } finally {
-            // Kaynakları kapat
-            try {
-                if (resultSet != null) resultSet.close();
-                if (statement != null) statement.close();
-                if (connection != null) connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
->>>>>>> 79042868a5963f99dd55dd82fbcee2069c4b2bd4
            // Formu başlat
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
