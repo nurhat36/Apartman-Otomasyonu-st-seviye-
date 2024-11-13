f9d5e4712257fb140aef2dc855e7b7620b074258
zahra Seyhnebi <zahraseyhnebi7@gmail.com>
2024-11-13 11:23:29 +0300

    aidat işlemini tamamladım 8

--- ApartmanOtomasyonu/src/main/java/com/mycompany/apartmanotomasyonu/ApartmanOtomasyonu.java
+++ ApartmanOtomasyonu/src/main/java/com/mycompany/apartmanotomasyonu/ApartmanOtomasyonu.java
@@ -5,18 +5,7 @@ public class ApartmanOtomasyonu {
 
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-=======
-       
-        
-        
-        
-        // Bağlantı bilgilerigre
-        String url = "jdbc:sqlserver://DESKTOP-T11FMIO;databaseName=APARTMAN;integratedSecurity=True;encrypt=True;trustServerCertificate=True";
-        Connection connection = null;
-        Statement statement = null;
-        ResultSet resultSet = null;
->>>>>>> e84f44b530989ad12539389dc118177dda20b07b
+
 
            // Formu başlat
         java.awt.EventQueue.invokeLater(new Runnable() {
